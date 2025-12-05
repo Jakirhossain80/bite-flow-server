dd427747bcadefa97ecdbc9fee820a93baead016
JakirHossain <jakir.devbd@gmail.com>
2025-12-05 10:01:38 +0600

    userModel has been updated

--- models/userModel.js
+++ models/userModel.js
@@ -27,13 +27,12 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+// Hash password before saving (async middleware, no `next`)
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Password comparison method
