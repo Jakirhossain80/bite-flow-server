d7c20e8450252b318261fba987ad11c88dd88d44
JakirHossain <jakir.devbd@gmail.com>
2025-12-15 08:09:42 +0600

    final codes

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-// index.js
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import connectDB from "./config/db.js";         
+import connectDB from "./config/db.js";
 import corsOptions from "./cors/corsOptions.js";
+
 import authRoutes from "./routes/authRoutes.js";
-import notFound from "./middlewares/notFound.js";
-import errorHandler from "./middlewares/errorHandler.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import menuRoutes from "./routes/menuRoutes.js";
@@ -16,28 +14,23 @@ import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 
+import notFound from "./middlewares/notFound.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
-// Load .env variables
 dotenv.config();
-
-// Connect to MongoDB Atlas
 connectDB();
 
 const app = express();
 
-// Middlewares
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-// Basic route
 app.get("/", (req, res) => {
   res.json({ message: "Restaurant API is running..." });
 });
 
-// Example routes/Base path
 app.use("/api/auth", authRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/category", categoryRoutes);
@@ -46,12 +39,7 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/booking", bookingRoutes);
 
-
-// 404 + error handlers
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+export default app;
